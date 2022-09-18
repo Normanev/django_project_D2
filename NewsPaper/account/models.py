e6fd08f9b049dbbf6208334e1295aa03daa27eed
Name <normanew@ya.ru>
2022-09-18 17:50:39 +0700

    2nd run

--- NewsPaper/account/models.py
+++ NewsPaper/account/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    pass
+# CREATE TABLE PRODUCTS (
+#     product_id INT AUTO_INCREMENT NOT NULL,
+#     name CHAR(255) NOT NULL,
+#     price FLOAT NOT NULL,
+#
+#     PRIMARY KEY (product_id)
+# );
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField(default=0.0)
+
+
+class Staff(models.Model):
+    pass
+
+
+class ProductOrder(models.Model):
+    pass
